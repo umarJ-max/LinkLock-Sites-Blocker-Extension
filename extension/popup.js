ea69fabad6eab726715a4ebbf837630cb8bdfc51
Umar <digitalcreatoruj@gmail.com>
2025-08-30 11:19:15 +0500

    Resolve the Extension problems

--- extension/popup.js
+++ extension/popup.js
@@ -14,7 +14,21 @@ document.getElementById('add').onclick = function() {
 function showSites() {
   chrome.storage.local.get(['blockedSites'], (result) => {
     const sites = result.blockedSites || [];
-    document.getElementById('sites').innerHTML = sites.map(s => `<div>${s}</div>`).join('');
+    document.getElementById('sites').innerHTML = sites.map((s, i) => 
+      `<div class="site-item">
+        <span>${s}</span>
+        <button class="remove-btn" onclick="removeSite(${i})">×</button>
+      </div>`
+    ).join('');
+  });
+}
+
+function removeSite(index) {
+  chrome.storage.local.get(['blockedSites'], (result) => {
+    const sites = result.blockedSites || [];
+    sites.splice(index, 1);
+    chrome.storage.local.set({blockedSites: sites});
+    showSites();
   });
 }
 
